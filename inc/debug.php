df3e6440e5fa50d3422662a7748c4db7d8500a54
Alex Koti <alexkoti@gmail.com>
2015-01-23 20:49:03 -0200

    update todo

--- inc/debug.php
+++ inc/debug.php
@@ -3,6 +3,7 @@
  * DEBUG CONFIG
  * Configurações de debug >>> NÂO CONFUNDIR COM /functions/debug.php !!!
  * 
+ * @todo - remover este arquivo, já que os debugs de rodapé padrão estão como opções de Boros e já existe o arquivo temp.php para testes
  */
 
 
