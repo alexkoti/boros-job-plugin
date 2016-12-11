0a8a9306d5ef1531e8410033ce08a8895d682af5
Alex Koti <alexkoti@gmail.com>
2016-12-11 20:59:37 -0200

    Link de settingsremovido
    Poderá ser readicionado conforme o projeto.

--- inc/admin-init.php
+++ inc/admin-init.php
@@ -1,18 +1,4 @@
 <?php
-/*
- * PLUGIN SETTINGS LINK
- * Link de opções na listagem de plugins
- * 
- */
-add_filter( 'plugin_action_links_boros_base/boros_base.php', 'my_add_settings_link', 10, 2 );
-function my_add_settings_link($links, $file){
-	$settings_link = sprintf( '<a href="admin.php?page=%s">%s</a>', 'section_general', __('Settings') );
-	array_unshift( $links, $settings_link );
-	return $links;
-}
-
-
-
 /**
  * ==================================================
  * WORK CSS/JS ======================================
